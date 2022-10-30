e738a2eb66965ff074fe7c0f34148cf9d5806ba0
Cristiano Marques <cristiano.marques@gmail.com>
2022-10-30 12:34:52 -0300

    meu primeiro commit
    Revolução do SAP Fiori

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,92 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel) {
+        "use strict";
+
+        return Controller.extend("revolucao.controller.Main", {
+            onInit: function () {
+                var ImageList = {
+                    Images: [
+                        {
+                           title: "Google",
+                           url: "https://www.google.com" 
+                        }
+
+                    ]
+                };
+
+                var ImageModel = new JSONModel(ImageList);
+                this.getView().setModel(ImageModel, "ModeloImagem");
+            },
+            onPressBuscar: function(){
+                //alert("começou a revolução do SAP Fiori!!");
+                var oInputBusca = this.byId("inpBusca");
+                var sQuery = oInputBusca.getValue();
+                //alert(sQuery);
+
+                $.ajax({
+                    //cabeçalho
+                    url: "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI",
+                    method: "GET",
+                    async:true,
+                    crossDomain:true,
+                    jsonpCallback: "getJSON",
+                    contentType: "application/json",
+                    headers: {
+                        "X-RapidAPI-Key": "8c65195ce1mshc3ee20d7cd99edfp116562jsn9d23bc39e195",
+		                "X-RapidAPI-Host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
+                    },
+
+                    //corpo
+                    data: {
+                        "q": sQuery,
+                        "pageNumber": 1,
+                        "pageSize": 30,
+                        "autoCorrect": true,
+                        "safeSearch" : true
+                    },
+                    //retorno em caso de sucesso
+                    success: function(data, textStatus){
+                        var oImageModel = this.getView().getModel("ModeloImagem");
+                        var oDadosImage = oImageModel.getData();
+
+                        oDadosImage = {
+                            Images: []
+                        };
+                        oImageModel.setData(oDadosImage);
+                        debugger
+
+                        var listaResultados = data.value;
+                        var newItem;
+
+                        for(var i = 0; i < listaResultados.length ; i++){
+                            
+                            newItem = listaResultados[i];
+                            oDadosImage.Images.push(newItem);
+
+                        };
+
+                        oImageModel.refresh();
+
+                        
+
+
+                    }.bind(this), 
+
+                    //retorno em caso de erro
+                    error: function(){
+
+                    }.bind(this)
+
+
+                });//fim  $.ajax({
+            }
+
+
+        });
+    });
